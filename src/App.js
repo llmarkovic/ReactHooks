5c98d8fa79c0b617c02abc7e771bd1870c4af37b
Luka Markovic <markovicluka29@gmail.com>
2020-09-19 13:44:33 +0200

    ToDo App through React Hooks (Not Finished yet)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Todo({ todo, index }) {
+  return <div className="todo">{ todo.text }</div>;
+}
+
+function TodoForm({ addTodo }) {
+  const [ value, setValue ] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={ e => setValue(e.target.value) }/>
+    </form>
+  )
+}
+
+function App() {
+  const [ todos, setTodos ] = useState([
+    {
+      text: 'Learn about React Hooks',
+      isCompleted: false
+    },
+    {
+      text: 'Go to Vlaska',
+      isCompleted: false
+    },
+    {
+      text: 'Workout and help my parents',
+      isCompleted: false
+    },
+  ])
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
